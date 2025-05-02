faa14480bb62892b8e25b612cc66f4fa551c05c9
Alima <alimausmani21@navgurukul>
2025-05-02 13:28:59 +0530

    Added Folder

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl text-blue-600 font-bold">Tailwind is working!</h1>
+      <hr className="my-6 border-t-2 border-blue-600" />
+    </div>
+  );
+};
+
+export default App;
